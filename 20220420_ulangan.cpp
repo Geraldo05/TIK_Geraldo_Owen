53be48749f5cef33f6ce976d7275026e8e5a6339
Geraldo05 <103085931+Geraldo05@users.noreply.github.com>
2022-04-20 13:38:16 +0800

    Update and rename 20220420_variable.cpp to 20220420_ulangan.cpp

--- 20220420_ulangan.cpp
+++ 20220420_ulangan.cpp
@@ -6,26 +6,26 @@ int main (){
     int nilai2;
     int nilai3;
     int nilaiakhir;
+
     cout<<"Masukkan Nilai Tugas = ";cin>>nilai1;
-    cout<<"Masukkan Nilai UTS = ";cin>>nilai2;
-    cout<<"Masukkan Nilai UAS = ";cin>>nilai3;
-    
-    if (nilai1>100){
+        if (nilai1>100){
         cout<<"Nilai tidak bisa melebihi 100";
         return 0;
-    
-    }
+        }
+        
+    cout<<"Masukkan Nilai UTS = ";cin>>nilai2;
     if (nilai2>100){
         cout<<"Nilai tidak bisa melebihi 100";
         return 0;
+        }
     
-    }
-    if (nilai3>100){
+    cout<<"Masukkan Nilai UAS = ";cin>>nilai3;
+        if (nilai3>100){
         cout<<"Nilai tidak bisa melebihi 100";
         return 0;
+        }
     
-    }
-    
+
     cout<<"Nilai = ";cout<<(nilai1*0.3)+(nilai2*0.3)+(nilai3*0.4);
 
 }
