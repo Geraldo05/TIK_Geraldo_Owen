c18ddff004f293b70db1bea585c11ef20a713848
Geraldo05 <103085931+Geraldo05@users.noreply.github.com>
2022-05-11 10:21:05 +0800

    Create 20220511_array_masukkan_angka.cpp

---
+++ 20220511_array_masukkan_angka.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int angka[] = {
+    };
+    for (int j = 0; j <5; j++){
+        cout<<"Masukkan angka = "; cin>>angka[j];
+    }
+    
+    for(int i = 0; i<5 ; i ++){
+        cout<<"Angka ke - "<<i<<" = "<<angka[i]*angka[i]<<endl;
+    }
+
+    return 0;
+}
